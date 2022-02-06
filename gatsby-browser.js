492f41c33543a4f346a965f854016e8c31bf2bf7
Emil Privér <emil@rivercode.se>
2022-02-06 20:04:14 +0100

    Added fonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,8 @@
 // custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
+import "@fontsource/dm-sans/400.css"
+import "@fontsource/dm-sans/500.css"
+import "@fontsource/dm-sans/700.css"
+
 
 import "./src/css/tailwind.css"
 
--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -81,11 +81,7 @@ const Seo: React.FC<Props> = ({ description, lang, meta, title, image }) => {
             content: metaDescription,
           },
         ].concat(meta ?? [])}
-      >
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap" rel="stylesheet"/>
-      </Helmet>
+      />
     )
 }
 
