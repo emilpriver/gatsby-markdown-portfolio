ca16a986b4056396fc5450033e99fe79c5cdd708
Emil Privér <emil@rivercode.se>
2022-03-05 13:03:29 +0100

    ADded buildtime

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,15 +1,32 @@
 import React from 'react'
+import {graphql, useStaticQuery} from "gatsby";
 
-const Footer: React.FC = () => (
-    <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a className="underline" href="https://www.gatsbyjs.com">Gatsby</a>
-        {' '}
-        -
-        {' '}
-        Hosted on <a className="underline" href="https://www.gatsbyjs.com/products/cloud/"> Gatsby Cloud</a>
-    </footer>
-)
+const Footer: React.FC = () => {
+    const query = useStaticQuery(graphql`
+        query {
+            site {
+                buildTime(fromNow: true)
+            }
+        }
+    `)
+
+    console.log(query)
+
+    return (
+        <footer>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a className="underline" href="https://www.gatsbyjs.com">Gatsby</a>
+            {' '}
+            -
+            {' '}
+            Hosted on <a className="underline" href="https://www.gatsbyjs.com/products/cloud/"> Gatsby Cloud</a>
+            {' '}
+            -
+            {' '}
+            This page was last build {query.site.buildTime}
+        </footer>
+    )
+}
 
 export default Footer
